fa68cccc1108d673ba3ee254a5dada2f138bd1c9
xumelo <alejuadra@alu.edu.gva.es>
2024-12-05 15:39:01 +0100

    prueba VerificarLetra

--- UNCOMPLETE_HANGMAN_GAME/VerificarLetra.java
+++ UNCOMPLETE_HANGMAN_GAME/VerificarLetra.java
@@ -1,9 +1,9 @@
 public class VerificarLetra {
-    public static boolean verificarLetra(String XXX, char XXX, StringBuilder letrasAdivinadas) {
-        if (palabra.indexOf(XXX) >= 0) {
-            letrasAdivinadas.append(XXX);
-            return XXX;
+    public static boolean verificarLetra(String palabra, char letra, StringBuilder letrasAdivinadas) {
+        if (palabra.indexOf(letra) >= 0) {
+            letrasAdivinadas.append(letra);
+            return true;
         }
-        return XXX;
+        return false;
     }
 }
